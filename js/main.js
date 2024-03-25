bd74546bafa50eccb39c81b0f344d89862a50462
ToosabiDev <162967309+ToosabiDev@users.noreply.github.com>
2024-03-25 12:16:17 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -290,6 +290,24 @@
             } else {
                 jQuery("#go-top").fadeOut(fadeOutTime);
             }
+            var slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
 
         }
 
